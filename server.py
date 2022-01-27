0fc358220e3e83ecaaa8f27f5a68937403c99280
manthankumbhar <kum.manthan@gmail.com>
2022-01-27 18:12:45 +0530

    tz removal

--- server.py
+++ server.py
@@ -28,10 +28,10 @@ class Users(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, unique=True, server_default=sqlalchemy.text("uuid_generate_v4()"),)
     email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    created_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=db.func.now(), server_onupdate=db.func.now())
+    created_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now(), server_onupdate=db.func.now())
     reset_password_hash = db.Column(db.String(64), unique=True, nullable=False)
-    reset_password_last_requested_at = db.Column(db.DateTime(timezone=True), nullable=True)
+    reset_password_last_requested_at = db.Column(db.DateTime(), nullable=True)
 
 def __init__(self, email, password, created_at, updated_at, reset_password_hash, reset_password_last_requested_at):
     self.email = email
