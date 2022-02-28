35a2f7c6acd13bb08bad2a325560d94b9377031d
manthankumbhar <kum.manthan@gmail.com>
2022-03-01 00:42:34 +0530

    comment changes

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import datetime
 import os
 from pyexpat import model
+import re
 from time import timezone
 from flask import Flask, jsonify, request, render_template
 from flask_bcrypt import Bcrypt
@@ -9,17 +10,18 @@ import secrets
 from flask_cors import CORS, cross_origin
 from models.User import db, User
 from sendgrid_helper import send_reset_password_mail
+from state_machine import create_token, create_user, get_user_by_email, token_valid_check, update_reset_password_token, update_user
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 cors = CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1) # sqlalchemy documents need the database name to include "ql" and this is the best way to remove it
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+# .replace("postgres://", "postgresql://", 1) # sqlalchemy documents need the database name to include "ql" and this is the best way to remove it
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 with app.app_context():
     db.create_all()
-
 app.config['ACCESS_TOKEN_SECRET'] =  os.environ.get('ACCESS_TOKEN_SECRET')
 app.config['REFRESH_TOKEN_SECRET'] =  os.environ.get('REFRESH_TOKEN_SECRET')
 app.config['SENDGRID_EMAIL'] = os.environ.get('SENDGRID_EMAIL')
@@ -28,26 +30,6 @@ app.config['SENDGRID_API_KEY'] = os.environ.get('SENDGRID_API_KEY')
 @app.route('/', methods=['POST', 'GET'])
 def hello_world():
     return jsonify({'success': 'Hello, World!'}), 200    
-
-def get_user_by_email(email):
-    users = db.session.query(User).filter(User.email == email)
-    if users.count() <= 0:
-        return None
-    if users.count() == 1:
-        for i in users:
-            return vars(i)
-    if users.count() > 1:
-        return {'error':'voilates the unique ability!'}, 500
-
-def token_valid_check(token):
-    users = db.session.query(User).filter(User.reset_password_hash == token)
-    if users.count() <= 0:
-        return None
-    if users.count() == 1:
-        for i in users:
-            return vars(i)
-    if users.count() > 1:
-        return {'error':'voilates the unique ability!'}, 500
     
 @app.route('/signup', methods=['POST'])
 def signup():
@@ -61,16 +43,10 @@ def signup():
         return jsonify({'error':'This email has an existing account.'}), 400    
     hashed_password = bcrypt.generate_password_hash(password, 10).decode('UTF-8')
     try:
-        user = User(
-            email = email,
-            password = hashed_password,
-        )
-        db.session.add(user)
-        db.session.commit()
+        create_user(email, hashed_password)
         user_id = get_user_by_email(email)['id']
-        accessToken = jwt.encode({'userid': str(user_id), 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['ACCESS_TOKEN_SECRET'])
-        refreshToken = jwt.encode({'userid': str(user_id), 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24*365)}, app.config['REFRESH_TOKEN_SECRET'])
-        return jsonify({'accessToken':accessToken.decode('UTF-8'), 'refreshToken': refreshToken.decode('UTF-8')}), 200
+        token = create_token(user_id)
+        return token, 200
     except Exception as e:
         return jsonify({'error': str(e.message)}), 500
 
@@ -86,9 +62,8 @@ def signin():
         return jsonify({'error':'This email does not have an existing account.'}), 400
     try:
         if bcrypt.check_password_hash(data_from_db['password'], password) == True:
-            accessToken = jwt.encode({'userid': str(data_from_db['id']), 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['ACCESS_TOKEN_SECRET'])            
-            refreshToken = jwt.encode({'userid': str(data_from_db['id']), 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24*365)}, app.config['REFRESH_TOKEN_SECRET'])            
-            return jsonify({'accessToken':accessToken.decode('UTF-8'), 'refreshToken': refreshToken.decode('UTF-8')}), 200
+            token = create_token(data_from_db['id'])
+            return token, 200
         else:
             return jsonify({'error':'Incorrect email or password.'}), 400
     except Exception as e:
@@ -97,9 +72,10 @@ def signin():
 @app.route('/reset-password', methods=['POST'])
 def send_reset_password_link():
     data = request.get_json()
-    if data['email'] == "" or None:
+    email = data['email']
+    if email == "" or None:
         return jsonify({'error':'email not entered'}), 400
-    data_from_db = get_user_by_email(data['email'])
+    data_from_db = get_user_by_email(email)
     if data_from_db is None:
         return jsonify({'error':'This email does not have an existing account.'}), 400
     reset_password_last_requested_at = data_from_db['reset_password_last_requested_at']
@@ -107,11 +83,8 @@ def send_reset_password_link():
     if reset_password_last_requested_at is None and reset_password_hash is None:        
         try:
             new_reset_password_hash = secrets.token_urlsafe(48)
-            user = db.session.query(User).filter(User.email == data['email']).first()
-            user.reset_password_last_requested_at = datetime.datetime.utcnow()
-            user.reset_password_hash = new_reset_password_hash
-            db.session.commit()
-            send_reset_password_mail(data['email'],new_reset_password_hash)
+            update_reset_password_token(email, new_reset_password_hash)
+            send_reset_password_mail(email,new_reset_password_hash)
             return jsonify({'success':'Check your inbox for the link to reset your password.'}), 200
         except Exception as e:
             return jsonify({'error':'Internal server error, please try again later'}), 500
@@ -120,17 +93,14 @@ def send_reset_password_link():
         if is_new_request is True:
             try:
                 updated_reset_password_hash = secrets.token_urlsafe(48)
-                user = db.session.query(User).filter(User.email == data['email']).first()
-                user.reset_password_last_requested_at = datetime.datetime.utcnow()
-                user.reset_password_hash = updated_reset_password_hash
-                db.session.commit()
-                send_reset_password_mail(data['email'],updated_reset_password_hash)
+                update_reset_password_token(email, updated_reset_password_hash)
+                send_reset_password_mail(email,updated_reset_password_hash)
                 return jsonify({'success':'Check your inbox for the link to reset your password.'}), 200
             except Exception as e:
                 return jsonify({'error':'Internal server error, please try again later'}), 500
         else:
             try:
-                send_reset_password_mail(data['email'],reset_password_hash)
+                send_reset_password_mail(email,reset_password_hash)
                 return jsonify({'success':'Check your inbox for the link to reset your password.'}), 200
             except Exception as e:
                 return jsonify({'error':'Internal server error, please try again later'}), 500
@@ -159,12 +129,9 @@ def post_reset_password(token):
     if new_password == "" or None:
         return jsonify({'error':'Password is not entered'}), 400
     try:
-        updated_reset_password_hash = secrets.token_urlsafe(48)
         hashed_new_password = bcrypt.generate_password_hash(new_password, 10).decode('UTF-8')
-        user = db.session.query(User).filter(User.email == data_from_db['email']).first()
-        user.reset_password_hash = updated_reset_password_hash
-        user.password = hashed_new_password
-        db.session.commit()
+        updated_reset_password_hash = secrets.token_urlsafe(48)
+        update_user(data_from_db['email'], hashed_new_password, updated_reset_password_hash)
         return jsonify({'success':'reset password successful!'}), 200
     except Exception as e:
         return jsonify({'error':str(e.message)}), 500
@@ -178,9 +145,8 @@ def refresh_tokens():
     try: 
         is_token_valid = jwt.decode(token_from_client, app.config['REFRESH_TOKEN_SECRET'], algorithms=["HS256"])
         user_id = is_token_valid['userid']
-        accessToken = jwt.encode({'userid': str(user_id), 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['ACCESS_TOKEN_SECRET'])            
-        refreshToken = jwt.encode({'userid': str(user_id), 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24*365)}, app.config['REFRESH_TOKEN_SECRET'])
-        return jsonify({'accessToken':accessToken.decode('UTF-8'), 'refreshToken': refreshToken.decode('UTF-8')}), 200
+        token = create_token(user_id)
+        return token, 200
     except:
         return jsonify({'error':'invalid token'}), 400
 
--- state_machine.py
+++ state_machine.py
@@ -1,16 +1,53 @@
+import datetime
+import os
 from flask import jsonify
-from models.User import User, SQLAlchemy
-from server import get_user_by_email
+from models.User import User, db
+import jwt
 
-db = SQLAlchemy()
+def get_user_by_email(email):
+    users = db.session.query(User).filter(User.email == email)
+    if users.count() <= 0:
+        return None
+    if users.count() == 1:
+        for i in users:
+            return vars(i)
+    if users.count() > 1:
+        return {'error':'voilates the unique ability!'}, 500
 
-def create_user(data):
+def token_valid_check(token):
+    users = db.session.query(User).filter(User.reset_password_hash == token)
+    if users.count() <= 0:
+        return None
+    if users.count() == 1:
+        for i in users:
+            return vars(i)
+    if users.count() > 1:
+        return {'error':'voilates the unique ability!'}, 500
+
+def create_user(email, password):
     user = User(
-        data
+        email = email,
+        password = password
     )
     db.session.add(user)
     db.session.commit()
 
+def create_token(user_id):
+    accessToken = jwt.encode({'userid': str(user_id), 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, os.environ.get('ACCESS_TOKEN_SECRET'))
+    refreshToken = jwt.encode({'userid': str(user_id), 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24*365)}, os.environ.get('REFRESH_TOKEN_SECRET'))
+    return jsonify({'accessToken':accessToken.decode('UTF-8'), 'refreshToken': refreshToken.decode('UTF-8')})
+
+def update_reset_password_token(email, reset_password_hash):
+    user = db.session.query(User).filter(User.email == email).first()
+    user.reset_password_last_requested_at = datetime.datetime.utcnow()
+    user.reset_password_hash = reset_password_hash
+    db.session.commit()
+
+def update_user(email, password, reset_password_hash):
+    user = db.session.query(User).filter(User.email == email).first()
+    user.password = password
+    user.reset_password_hash = reset_password_hash
+    db.session.commit()
     
 
 
