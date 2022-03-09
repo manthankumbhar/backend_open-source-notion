f7d6e32ffe2e928441987b994c89855d0511c611
manthankumbhar <kum.manthan@gmail.com>
2022-03-09 15:59:29 +0530

    config changes

--- config.py
+++ config.py
@@ -1,16 +1,20 @@
 import os
 
-config = {
-    'DATABASE_URL': os.environ.get('DATABASE_URL'),
-    'DATABASE_TRACK_MODIFICATIONS':os.environ.get('DATABASE_TRACK_MODIFICATIONS'),
-    'ACCESS_TOKEN_SECRET':os.environ.get('ACCESS_TOKEN_SECRET'),
-    'REFRESH_TOKEN_SECRET':os.environ.get('REFRESH_TOKEN_SECRET'),
-    'SENDGRID_EMAIL':os.environ.get('SENDGRID_EMAIL'),
-    'SENDGRID_API_KEY':os.environ.get('SENDGRID_API_KEY'),
-    'SENDGRID_SERVER_LINK':os.environ.get('SENDGRID_SERVER_LINK'),
-    'DEBUG': os.environ.get('DEBUG'),
-}
+env_list = [
+    'DATABASE_URL', 
+    'DATABASE_TRACK_MODIFICATIONS',
+    'ACCESS_TOKEN_SECRET',
+    'REFRESH_TOKEN_SECRET',
+    'SENDGRID_EMAIL',
+    'SENDGRID_API_KEY',
+    'SENDGRID_SERVER_LINK',
+    'DEBUG', 
+]
 
-for i in config:
+for i in env_list:
     if i not in os.environ:
-        raise Exception({'error':'environment variable not defined'})
+        raise Exception({'error':'%s - environment variable not defined' % i})
+
+config = {}
+for i in env_list:
+    config[i] = os.environ.get(i)
