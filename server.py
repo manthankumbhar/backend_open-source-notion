76637b650f73af683372c5f7d3c29902f91ba4d1
manthankumbhar <kum.manthan@gmail.com>
2022-03-09 16:33:58 +0530

    postgres err debug

--- server.py
+++ server.py
@@ -8,8 +8,7 @@ app = Flask(__name__)
 app.register_blueprint(resources, url_prefix="")
 cors = CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = config['DATABASE_URL']
-# .replace("postgres://", "postgresql://", 1) # sqlalchemy documents need the database name to include "ql" and this is the best way to remove it
+app.config['SQLALCHEMY_DATABASE_URI'] = config['DATABASE_URL'].replace("postgres://", "postgresql://", 1) # sqlalchemy documents need the database name to include "ql" and this is the best way to remove it
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = config['DATABASE_TRACK_MODIFICATIONS']
 db.init_app(app)
 with app.app_context():
